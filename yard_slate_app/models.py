69895e01f323246c82a05c5f32e6a94ba7973116
akittredge <andrewlkittredge@gmail.com>
2012-07-14 20:55:40 -0400

    adding new model

--- yard_slate_app/models.py
+++ yard_slate_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class YardSlate(models.Model):
+    title = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
