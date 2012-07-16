ee7ad1917f74daad7890e8acf4cc675fb7c147f9
akittredge <andrewlkittredge@gmail.com>
2012-07-15 22:55:46 -0400

    initial form is working

--- yard_slate_app/views.py
+++ yard_slate_app/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from django.forms import ModelForm
 from collections import namedtuple
+from yard_slate_app.models import YardSlate
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 def index(request):
     return render_to_response('index.html')
 
 def create(request):
-    return render_to_response('create.html')
+    if request.method == 'POST':
+        form = SlateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/view_slate/')
+    else:
+        form = SlateForm()
+    return render(request, 'create.html', {'form' : form})
+    
+
+
+class SlateForm(ModelForm):
+    class Meta:
+        model = YardSlate
 
 def view_slate(request):
-    slate = Slate(123)
-    return render_to_response('view_slate.html', {'slate' : slate})
+            
+    return render(request, 'view_slate.html')
+    
+
 
 Slate = namedtuple('slate', ('id'))
