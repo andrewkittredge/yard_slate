17b33c1dc150212ea5406ffb93cc6b9a2be00f8b
akittredge <andrewlkittredge@gmail.com>
2012-07-20 23:32:17 -0400

    got images serving working

--- yard_slate/urls.py
+++ yard_slate/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from yard_slate import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,3 +21,6 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('', (r'^slate_images/(?P<path>.*)$', 'django.views.static.serve',
+                            {'document_root' : settings.MEDIA_ROOT,
+                             'show_indexes' : True}))
