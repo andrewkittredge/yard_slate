b1c10ca6eb813fd791e6668c590e16056831b44a
akittredge <andrewlkittredge@gmail.com>
2012-07-28 08:50:24 -0400

    adding location field

--- yard_slate_app/models.py
+++ yard_slate_app/models.py
@@ -5,6 +5,8 @@ class YardSlate(models.Model):
     title = models.CharField(max_length=50)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    
     photo_one = models.ImageField(upload_to='./')
     photo_two = models.ImageField(upload_to='slate_images')
     photo_three = models.ImageField(upload_to='slate_images')
