dbbae10746da3bb7458cce643df0670123a24097
akittredge <andrewlkittredge@gmail.com>
2012-08-04 00:54:37 +0000

    moving templates dirs to local settings

--- settings.py
+++ settings.py
@@ -96,13 +96,6 @@ ROOT_URLCONF = 'yard_slate.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'yard_slate.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/Users/akittredge/Documents/workspace/yard_slate/templates'
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
