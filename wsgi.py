12532d76085e307b9c152dc322e064909e3f44c3
akittredge <andrewlkittredge@gmail.com>
2012-08-04 01:31:09 +0000

    adding to sys.path

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,7 @@ import sys
 
 from django.core.handlers.wsgi import WSGIHandler
 
+sys.path.append('/home/yardslate/webapps/yard_slate/myproject/myproject')
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'yard_slate.settings'
 application = WSGIHandler()
