eee5e2da1fff9cc770590b8e229e65794aabd20a
akittredge <andrewlkittredge@gmail.com>
2012-08-11 19:01:27 -0400

    directions are working.

--- yard_slate_app/static/scripts/global.js
+++ yard_slate_app/static/scripts/global.js
@@ -11,11 +11,16 @@ YSLATE.common = (function() {
 }());
 
 YSLATE.mobile = (function() {
-
+	var directionsDisplay;
 	var init = function() {
-		alert ( $(window).width() );
+		//alert ( $(window).width() );
+		directionsDisplay = new google.maps.DirectionsRenderer();
 		setupMap();
+		calcRoute();
+		
 	};
+	
+	
 
 	var setupMap = function() {
 
@@ -27,6 +32,7 @@ YSLATE.mobile = (function() {
 		var address = window.SLATELOCATION;
 		var map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
 		var geocoder = new google.maps.Geocoder();
+		directionsDisplay.setMap(map);
 		
         geocoder.geocode( { 'address' : address }, showSinglePoint);
 
@@ -41,8 +47,35 @@ YSLATE.mobile = (function() {
 				console.log('Geocode was not successful for the following reason: ' + status);
 			}
         }
-
 	};
+	
+	function calcRoute() {
+		success = function(data) {
+			var latLong = new google.maps.LatLng(data.coords.latitude, 
+												 data.coords.longitude);
+			var directionsService = new google.maps.DirectionsService();
+			var start = latLong;
+			var end = "16 Adelaide Street Boston MA 02130";
+			var request = {
+					origin : start,
+					destination : end,
+					travelMode : google.maps.TravelMode.WALKING
+			};
+			directionsService.route(request, function(result, status){
+				if (status == google.maps.DirectionsStatus.OK){
+					directionsDisplay.setDirections(result);
+				}
+			});
+			alert(latLong)
+		};
+		var failure = function(error){
+			alert( "could not get location");
+		};
+		navigator.geolocation.getCurrentPosition(success, failure, {timeout : 5000});
+	};
+	
+	
+	
 
 	return {
 		init: init
